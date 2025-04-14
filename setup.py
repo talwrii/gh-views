9db0389fafc17bc9e1e3daab4f093a16326baf29
Tal Wrii <talwrii@gmail.com>
2025-04-14 21:50:10 +0200

    bump release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='gh-views',
-    version="1.0.3",
+    version="1.0.4",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
