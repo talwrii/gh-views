8ac5fc6ce65f4ba7c8cb65abc3828222811f9463
Tal Wrii <talwrii@gmail.com>
2025-04-18 22:31:29 +0200

    Do not save todays data. If run with --fetch include todays data

--- gh_views/main.py
+++ gh_views/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import argparse
-import datetime
 import json
 import logging
 import os
@@ -9,20 +8,20 @@ import subprocess
 import sys
 from pathlib import Path
 
-
-import pytz
-
 PARSER = argparse.ArgumentParser(description='Get information about github download stats')
+action_mx = PARSER.add_mutually_exclusive_group()
 PARSER.add_argument('repos', nargs="*", help="Repository to get data for. Omit to list repositories")
 PARSER.add_argument("-f", "--fetch", action="store_true", help="Fetch data. Omit repo to fetch all data.")
 PARSER.add_argument("--debug", action="store_true", help="Include debug output")
 PARSER.add_argument("-a", "--all", action="store_true", help="Show data for all repos")
-PARSER.add_argument("--delete", action="store_true", help="Delete data related to and stop fetching data for a repository")
 PARSER.add_argument("--json", action='store_true', help="Output results in json")
+PARSER.add_argument("--common-start", action='store_true', help="When dealing with multiple repos use the latest start date")
 
-PARSER.add_argument("-t", "--timeseries", action="store_true", help="Output a timeseries rather than summary")
-
-UTC = pytz.timezone("UTC")
+action_mx.add_argument("--delete", action="store_true",
+                       help="Delete data related to and stop fetching data for a repository")
+action_mx.add_argument("-t", "--timeseries", action="store_true", help="Output a timeseries rather than summary")
+action_mx.add_argument("-p", "--path",  action='store_true', help="Output the config path")
+action_mx.add_argument("--last-fetch",  action='store_true', help="Output the last fetch of a repo (or the least recent last fetch of all repos if no repo is provided)")
 
 STATS_PATH = Path(os.path.expanduser("~")) / ".local" / "state" / "gh-views"
 
@@ -61,6 +60,18 @@ def get_repos():
 
 def main():
     args = PARSER.parse_args()
+
+    if args.last_fetch:
+        repos = args.repos or get_repos()
+        print(min(get_fetch(r) for r in repos))
+        return
+
+    if args.path:
+        print(str(STATS_PATH))
+        return
+
+
+    todays_data = {}
     STATS_PATH.mkdir(exist_ok=True)
 
     if args.debug:
@@ -75,8 +86,6 @@ def main():
     else:
         display_func = display_summary
 
-
-
     if args.delete:
         if args.all:
             print("Refusing to delete all data.")
@@ -90,11 +99,12 @@ def main():
             delete(r)
         return
 
+
     if not args.repos:
         if args.fetch:
             repos = list(get_repos())
             for r in repos:
-                fetch(r)
+                todays_data[r] = fetch(r)
         elif args.all:
             for r in sorted(list(get_repos())):
                 print(r)
@@ -108,43 +118,106 @@ def main():
 
     if args.fetch:
         for r in args.repos:
-            print(r)
-            fetch(r)
+            todays_data[r] = fetch(r)
+
+    if args.common_start:
+        start = max(get_start(r) for r in args.repos)
+    else:
+        start = None
 
     if args.json:
-        print(json.dumps([display_summary_data(r) for r in args.repos], indent=2))
+        result = []
+        for r in args.repos:
+            series = read_series(r)
+            if todays_data:
+                add_data(series, todays_data[r])
+
+            result.append(display_summary_data(r, series, start=start), indent=2)
+        print(json.dumps(result))
     else:
         for r in args.repos:
-            display_func(r)
+
+            series = read_series(r)
+            if todays_data:
+                add_data(series, todays_data[r])
+
+            display_func(r, series=series, start=start)
+
+
+def add_data(series, data):
+    for k, v in data.items():
+        series[k].append(v)
+
 
 
 def delete(repo):
-    for d in [clone_path(repo), views_path(repo)]:
+    for d in [fetch_path(repo), clone_path(repo), views_path(repo)]:
         d.unlink()
 
+def get_fetch(repo):
+    return read_timeseries(fetch_path(repo))[-1]["timestamp"]
 
-def display_summary_data(repo):
-    clone_ts = read_timeseries(clone_path(repo))
-    view_ts = read_timeseries(views_path(repo))
+def get_start(repo) -> str:
+    if not os.path.exists(fetch_path(repo)):
+        raise Exception('Fetch data does not exist (added in 2.1.0) rerun --fetch. This means old data is ignored. If you care tweak the fetch file by hand.')
 
-    if not clone_ts and not view_ts:
-        print(f"Have not fetched data for {repo} yet.")
-        return None
+    ts = get_fetch(repo)
+    fetch_ts = NaiveUtcDate.parse(ts)
 
-    clone_start = min(d["timestamp"] for d in clone_ts) if clone_ts else None
-    view_start = min(d["timestamp"] for d in view_ts) if view_ts else None
+    window_ts = (fetch_ts.replace(hour=0, minute=0, second=0, microsecond=0) - NaiveUtcDate.timedelta(days=12))
 
-    if clone_start and view_start:
-        start = max([clone_start, view_start])
-    else:
-        start = clone_start or view_start
+    return window_ts.isoformat() + "Z"
 
-    clone_ts = ts_after(start, clone_ts)
-    view_ts = ts_after(start, view_ts)
 
-    start_dt = datetime.datetime.fromisoformat(start)
+class NaiveUtcDate:
+    # This mostly exists work work around the fact that python will not add Z's
+    # for the utc timezone and that is the format we get dates in
 
-    days = (UTC.localize(datetime.datetime.utcnow()) - start_dt).days
+    from datetime import timedelta
+    timedelta = timedelta
+
+    @classmethod
+    def now(cls):
+        import datetime
+        return datetime.datetime.utcnow().replace(tzinfo=None)
+
+    @classmethod
+    def format(cls, dt):
+        if dt.tzinfo != None:
+            raise Exception(f"{dt} has timezone")
+
+        return dt.isoformat() + "Z"
+
+    @classmethod
+    def parse(cls, s: str):
+        import datetime
+        if not (s[-2].isdigit() and s[-1] == "Z"):
+            raise Exception(f'timestamp ({s}) is not in UTC')
+
+        return datetime.datetime.fromisoformat(s).replace(tzinfo=None)
+
+
+def read_series(repo):
+    return {
+        "clones": read_timeseries(clone_path(repo)),
+        "views": read_timeseries(views_path(repo))
+        }
+
+
+def display_summary_data(repo, series, start):
+    repo_start = get_start(repo)
+    if start and start < repo_start:
+        raise Exception('start {start} must be after window for repo data (started at {start})')
+
+    start = start or repo_start
+
+    series = series_after(start, series)
+    view_ts = series["views"]
+    clone_ts = series["clones"]
+
+    start_dt = NaiveUtcDate.parse(start)
+
+    days = (NaiveUtcDate.now() - start_dt).days
 
     return {
         "repo": repo,
@@ -160,38 +233,30 @@ def display_summary_data(repo):
         "total_views_per_day": round(total(view_ts) / days, 3),
     }
 
-def display_summary(repo):
-    data = display_summary_data(repo)
+def display_summary(repo, series, start=None):
+    data = display_summary_data(repo, series, start=start)
     if data is None:
         return
     print(f"Since {data['start']} ({data['days']} days)")
     print(f"VIEWS  unique:{data['unique_views']} ({data['unique_views_per_day']:.1f} per day) total:{data['total_views']} ({data['total_views_per_day']:.1f} per day)")
     print(f"CLONES unique:{data['unique_clones']} ({data['unique_clones_per_day']:.1f} per day) total:{data['total_clones']} ({data['total_clones_per_day']:.1f} per day)")
 
-def display_summary_json(repo):
-    data = display_summary_data(repo)
+def display_summary_json(repo, series):
+    data = display_summary_data(repo, series)
     print(json.dumps(data, indent=2))
 
-def display_timeseries(repo):
-    cp = clone_path(repo)
-    clone_ts = read_timeseries(cp) if cp.exists() else []
+def display_timeseries(repo, series, start=None):
+    repo_start = get_start(repo)
+    if start and start < repo_start:
+        raise Exception(f'start ({start}) must be after start of repo data ({repo_start})')
 
-    vp = views_path(repo)
-    view_ts = read_timeseries(vp) if vp.exists() else []
+    start = start or repo_start
 
-    clone_start = min(d["timestamp"] for d in clone_ts) if clone_ts else None
-    view_start = min(d["timestamp"] for d in view_ts) if view_ts else None
-
-    if clone_start and view_start:
-        start = max([clone_start, view_start])
-    else:
-        start = clone_start or view_start
-
-    clone_ts = ts_after(start, clone_ts)
-    view_ts = ts_after(start, view_ts)
+    clone_ts = ts_after(start, series["clones"])
+    view_ts = ts_after(start, series["views"])
 
     for update in merge(dict(clone=clone_ts, views=view_ts)):
-        print(json.dumps(update))
+        print(json.dumps(dict(repo=repo, **update)))
 
 def merge(series):
     "Merge series together"
@@ -228,19 +293,23 @@ def merge(series):
 def ts_after(start, ts):
     return [d for d in ts if d["timestamp"] > start]
 
+def series_after(start, series):
+    return {k: ts_after(start, v) for k, v in series.items()}
+
 
 def fetch_clones(repo):
     data = fetch_data(f"repos/{repo}/traffic/clones")
-
     output = clone_path(repo)
     logging.debug("Writing to %r", output)
     update_timeseries(output, data["clones"])
+    return data["clones"][-1]
 
 def fetch_views(repo):
     data = fetch_data(f"repos/{repo}/traffic/views")
     output = views_path(repo)
     logging.debug("Writing to %r", output)
     update_timeseries(output, data["views"])
+    return data["views"][-1]
 
 
 def update_timeseries(path, new_ts):
@@ -252,6 +321,10 @@ def update_timeseries(path, new_ts):
     timestamps = ts and set(d["timestamp"] for d in ts)
     with open(path, "a") as f:
         for x in new_ts:
+            ts = NaiveUtcDate.parse(x["timestamp"])
+            if ts.date() == NaiveUtcDate.now().date():
+                # Exclude today because it could change
+                continue
             if not timestamps or x["timestamp"] not in timestamps:
                 f.write(json.dumps(x) + "\n")
 
@@ -259,12 +332,15 @@ def fetch_data(url):
     return json.loads(subprocess.check_output(["gh", "api", url]))
 
 
-def clone_path(repo):
+
+def clone_path(repo) -> Path:
     return STATS_PATH / (repo.replace("/", "--") + "--" + "clones.jsonl")
 
-
-def views_path(repo):
+def views_path(repo) -> Path:
     return STATS_PATH / (repo.replace("/", "--") + "--" + "views.jsonl")
+
+def fetch_path(repo) -> Path:
+    return STATS_PATH / (repo.replace("/", "--") + "--fetchs.jsonl")
 
 
 def uniques(ts):
@@ -274,11 +350,23 @@ def total(ts):
     return sum(d["count"] for d in ts)
 
 
-def fetch(repo):
+def fetch(repo: str):
+    record_fetch(repo)
     logging.info("Fetching clones for %r...", repo)
-    fetch_clones(repo)
+    todays_clones = fetch_clones(repo)
     logging.info("Fetching downloads for %r...", repo)
-    fetch_views(repo)
+    todays_views = fetch_views(repo)
+    return {"clones": todays_clones, "views": todays_views}
+
+
+
+
+def record_fetch(repo: str):
+    path = fetch_path(repo)
+    ts = NaiveUtcDate.now()
+    with open(path, "a") as f:
+        f.write(json.dumps({"timestamp": NaiveUtcDate.format(ts)}) + "\n")
+
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setuptools.setup(
     keywords='github, statistics',
     url='https://github.com/talwrii/gh-views',
     packages=["gh_views"],
-    install_requires=["pytz"],
     long_description=open('README.md').read(),
     entry_points={
         'console_scripts': ['gh-views=gh_views.main:main']
