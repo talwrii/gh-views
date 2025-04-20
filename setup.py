d2fb71cd19638166e56f6262054f8f1f648013a4
Tal Wrii <talwrii@gmail.com>
2025-04-20 17:05:43 +0200

    release new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='gh-views',
-    version="3.0.2",
+    version="3.1.0",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
